4d710a47e4cba313b6722293fae29e7f63aa73b3
Ingmar Vierhaus <vierhaus@zib.de>
2016-08-22 12:40:48 +0200

    Updated run_test.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 make test TEST=simulate SETTINGS=simulate  OPT=dbg LPS=cpx IPOPT=true
+read -p "Done with testet __simulate__. Press enter to continue" yn
+
+
+make test TEST=propODE SETTINGS=propODE  TIME=10  OPT=dbg LPS=cpx IPOPT=true
