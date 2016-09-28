bf2fcc2dc3338a4e3bb7f65be3463ba753ef8872
Ingmar Vierhaus <vierhaus@zib.de>
2016-09-28 18:12:49 +0200

    Reset pp model final time, TESTINGFINE scip 774e0d8

--- run_tests.sh
+++ run_tests.sh
@@ -4,14 +4,16 @@ if [ -z ${FLAGS+x} ]; then
     FLAGS="OPT=dbg LPS=cpx IPOPT=true ZIMPL=false"
 fi
 
-#echo make test TEST=simulate2 SETTINGS=simulate "$FLAGS"
-#make test TEST=simulate SETTINGS=simulate $FLAGS
+log="logs/tests_current.log"
 
+cat /dev/null > $log
+
+make test TEST=simulate SETTINGS=simulate $FLAGS | tee -a $log
 #read -p "Done with testset __simulate__. Press enter to continue" yn
 
-#make test TEST=propODE SETTINGS=propODE  TIME=10 $FLAGS
+make test TEST=propODE SETTINGS=propODE  TIME=10 $FLAGS  | tee -a $log
 #read -p "Done with testset __simulate__. Press enter to continue" yn
 
-make test TEST=obra SETTINGS=obra  $FLAGS
+make test TEST=obra SETTINGS=obra  $FLAGS  | tee -a $log
 #read -p "Done with testset __simulate__. Press enter to continue" yn
 
