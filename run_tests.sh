3af6e61a81848296c5ff3119a9daa9ab0bcfa91d
Ingmar Vierhaus <vierhaus@zib.de>
2016-09-28 17:02:14 +0200

    TESTINGFINE only sim and obra with scip 774e0d860fcb9199b0d4fa041f37599d233c157d

--- run_tests.sh
+++ run_tests.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-if [ $FLAGS = "" ]; then
+if [ -z ${FLAGS+x} ]; then
     FLAGS="OPT=dbg LPS=cpx IPOPT=true ZIMPL=false"
 fi
 
-make test TEST=simulate SETTINGS=simulate $FLAGS
+#echo make test TEST=simulate2 SETTINGS=simulate "$FLAGS"
+#make test TEST=simulate SETTINGS=simulate $FLAGS
+
 #read -p "Done with testset __simulate__. Press enter to continue" yn
 
-make test TEST=propODE SETTINGS=propODE  TIME=10 $FLAGS
+#make test TEST=propODE SETTINGS=propODE  TIME=10 $FLAGS
 #read -p "Done with testset __simulate__. Press enter to continue" yn
 
 make test TEST=obra SETTINGS=obra  $FLAGS
