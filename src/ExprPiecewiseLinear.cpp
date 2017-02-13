38b95e721f0242bc35712e21037d4e699ef22d5c
Ingmar Vierhaus <vierhaus@zib.de>
2017-02-13 16:21:29 +0100

    fix a bug in estimator selection

--- src/ExprPiecewiseLinear.cpp
+++ src/ExprPiecewiseLinear.cpp
@@ -692,10 +692,10 @@ static SCIP_DECL_USEREXPRESTIMATE( estimateLookup )
             SCIP_Real y1 = it->second;
             SCIP_Real y2 = (it+1)->second;
             /* Check availability of class-2 estimators ( $\underline x = x1 or \bar x = x2$) */
-            if (is_equal(x2, lb))
+            if (is_equal(x2, ub))
             {
                e5valid = TRUE;
-               SCIPdbgMsg("(x2,y1) = (%e,%e), ub = %e, e5 available\n", x2, y2, ub);
+               SCIPdbgMsg("(x2,y2) = (%e,%e), ub = %e, e5 available\n", x2, y2, ub);
             }
 
             if (is_equal(x1, lb))
