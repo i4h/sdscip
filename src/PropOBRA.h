75040a004aa567fe068e800b03c273ac323712c0
Ingmar Vierhaus <vierhaus@zib.de>
2017-03-09 15:52:16 +0100

    modify default subscip gap limit

--- src/PropOBRA.h
+++ src/PropOBRA.h
@@ -237,7 +237,7 @@ public:
       SCIPaddRealParam(scip,
             "propagating/obra/subscipGapLimit",
             "Gap Limit for the subscips",
-            &subscipGapLimit_, FALSE, 0.01, 0.0, SCIP_REAL_MAX, NULL, NULL);
+            &subscipGapLimit_, FALSE, 0.001, 0.0, SCIP_REAL_MAX, NULL, NULL);
 
       SCIPaddRealParam(scip,
             "propagating/obra/subscipTimeLimit",
